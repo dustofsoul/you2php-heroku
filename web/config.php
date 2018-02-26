801d4e617bc31fa71f4555b27b874556a8a5e031
dustofsoul <lhzlovekyf@gmail.com>
2018-02-26 16:17:13 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', 'AIzaSyCzyxhBV_Sp8pPbjjKjb6jr11iH6TtCh_A');//这里改成您的api key
+define('GJ_CODE', 'US');//国家代码
+define('SITE_NAME', '404');//网站名字
+define('TITLENAME', '404');//网站名字
+define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
+define('EMAIL', 'lll.lll@workmail.com');//邮箱
 ?>
